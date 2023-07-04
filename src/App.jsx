d1b21a29887542d128b584664de7d48406011346
vd89 <dixitvora@gmail.com>
2023-07-04 19:54:53 +0530

    EPP-284: added the changes for the card and getting the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,51 @@
-import { useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { Container, Grid, Typography } from '@mui/material'
-import { StudentSection, TabElement } from './Components'
+import { StudentSection, TabElement, TabValueSection } from './Components'
+import axios from 'axios'
 
 function App() {
-  const [getTabIndex, setTabIndex] = useState('one')
+  const [tabIndex, setTabIndex] = useState('one')
+  const [studentData, setStudentData] = useState([])
+  const token = 'GjlxunlpmjneBGrodv96Eg'
+
+  const getStudentData = useCallback(async (_token) => {
+    return await axios.get('https://web.testing.interstride.com/api/v1/admission_portal/students?&user_type=Student', {
+      headers: {
+        'accept-language': 'en-US,en;q=0.9',
+        accept: 'application/json, text/plain, */*',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+        token: `${_token}`,
+      },
+    })
+  }, [])
 
   const getTabValue = (_id) => {
     setTabIndex(_id)
     console.log('tabIndex', _id)
   }
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await getStudentData(token)
+      if (data.message === 'Success') {
+        setStudentData(data.data.users)
+      } else {
+        setStudentData([])
+      }
+    }
+    getData()
+    return () => {
+      getData()
+    }
+  }, [])
+
   return (
     <Container>
       <TabElement getTabValue={getTabValue} />
       <StudentSection />
+      <TabValueSection tabIndex={tabIndex} studentData={studentData} />
     </Container>
   )
 }
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,2 +1,4 @@
 export { default as TabElement } from './TabElement'
 export { default as StudentSection } from './StudentSection'
+export { default as TabValueSection } from './TabValueSection'
+export { default as StudentCard } from './StudentCard'
